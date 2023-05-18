af05c31fffb74a213bf25d680cdbdf2486fe404c
WataruSugimoto <wataru.s0208@gmail.com>
2023-05-18 23:05:26 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" width="25%" />
+    <List :childListData="listData" />
+  </div>
+</template>
+
+<script>
+import { ref } from "@vue/composition-api";
+import List from "./components/List";
+
+export default {
+  name: "App",
+  components: {
+    List,
+  },
+  setup() {
+    const message = ref("Hello World");
+    const listData = [
+      {
+        id: 1,
+        name: "りんご",
+      },
+      {
+        id: 2,
+        name: "メロン",
+      },
+      {
+        id: 3,
+        name: "ぶどう",
+      },
+    ];
+    return {
+      message,
+      listData,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,48 @@
+<template>
+  <ul class="List">
+    <li
+      v-for="childListItem in childListData"
+      :key="childListItem.id"
+      class="ListItem"
+    >
+      <p class="ListItemNumber">番号：{{ childListItem.id }}</p>
+      <p class="ListItemName">名前：{{ childListItem.name }}</p>
+    </li>
+  </ul>
+</template>
+
+<script>
+import { ref } from "@vue/composition-api";
+
+export default {
+  props: {
+    childListData: {
+      default: [],
+      required: true,
+    },
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+.List {
+  border: solid 1px black;
+  padding: 16px;
+}
+
+.ListItem {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  list-style: none;
+  width: 300px;
+  height: 80px;
+  background-color: #E0FFC1;
+  margin-bottom: 16px;
+  padding: 16px;
+}
+</style>
